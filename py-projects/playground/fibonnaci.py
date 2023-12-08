a8acc564d6a5807bbd2a97b1b6ee196f7f2a9cc4
Chibuzo Kelechi <narutokelechi@gmail.com>
2023-12-08 20:23:06 +0100

    fibonacci sequence code

--- py-projects/playground/fibonnaci.py
+++ py-projects/playground/fibonnaci.py
@@ -1,8 +1,18 @@
 nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 fibonacci_output = []
 
-for num in nums:
-    print(num + 1)
+def add_adj_nums():
+    fs_value = nums[0] + nums[1]
+    fibonacci_output.append(fs_value)
+
     
 def fibonnaci():
-    return num
+    fibonacci_output.append(nums[1])
+    while len(nums) > 1:
+        add_adj_nums()
+        del nums[0]
+        
+fibonnaci()
+
+for fs_num in fibonacci_output:
+        print(fs_num)
--- py-projects/playground/play..py
+++ py-projects/playground/play..py
@@ -1,36 +1,34 @@
 # Anime characters
-anime = 'Eminence in Shadow'
-xter = 'Cid'
+anime = "Eminence in Shadow"
+xter = "Cid"
 xter += "/Lord Shadow"
 
-manga = 'Jujutsu Kaisen'
-jjk_mc = 'Itadori Yuji'
+manga = "Jujutsu Kaisen"
+jjk_mc = "Itadori Yuji"
 
-jjk_characters = {
-    'strongest': 'Gojo Satoru',
-    'strongest2': 'Sukuna'
-}
+jjk_characters = {"strongest": "Gojo Satoru", "strongest2": "Sukuna"}
 
-users = ['Kc', 'Victor', 'Onyii', 'Chizzy', 'Daneasy']
+users = ["Kc", "Victor", "Onyii", "Chizzy", "Daneasy"]
 
 i = 3
 print(xter)
 # print((3*2) / 2 + 3)
 
-name = ''
+name = ""
 total = 0
 for num in range(101):
     total += num
     print(total)
-print(f'The total is {total}')
+print(f"The total is {total}")
+
 
 def divide(num):
-    return 9/num
+    return 9 / num
+
 
 try:
     print(divide(3))
     print(divide(0))
     print(divide(9))
 except ZeroDivisionError:
-    print('Zero gives an error')
-     
+    print("Zero gives an error")
