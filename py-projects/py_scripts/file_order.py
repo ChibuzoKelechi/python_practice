9f6a37601f900ad44ce1f50aeb6476f80ea537c5
Chibuzo Kelechi <narutokelechi@gmail.com>
2023-12-13 01:03:11 +0100

    Fixed a directory  bug

--- py-projects/py_scripts/file_order.py
+++ py-projects/py_scripts/file_order.py
@@ -6,55 +6,45 @@ import os, shutil
 downloads_folder = '/home/kelechi/Downloads'
 download_files = os.listdir(downloads_folder)
 
-
 def checkfile():
-    file_types = {
-        '.txt': 'textfiles',
-        '.jpg': 'Images',
-        '.png': 'Images',
-        '.pdf': 'PDFs',
-        '.zip': 'zipfiles',
-        '.rpm': 'rpm',
-    }
-
     for file in download_files:
         filepath = os.path.join(downloads_folder, file)
-        extension = os.path.splitext(file)[1]
-
-        destination_folder = file_types.get(extension, 'others')
-        destination_path = os.path.join(downloads_folder, destination_folder)
-
-        shutil.move(filepath, destination_path)
-
+        
+        if file.endswith('.txt'):
+            shutil.move(filepath, '/home/kelechi/Downloads/textfiles')
+        elif file.endswith('.jpg') or file.endswith('.png') or file.endswith('.webp'):
+            shutil.move(filepath, '/home/kelechi/Downloads/Images')
+        elif file.endswith('.pdf'):
+            shutil.move(filepath, '/home/kelechi/Downloads/PDFs')
+        elif file.endswith('.zip') or file.endswith('z'):
+            shutil.move(filepath, '/home/kelechi/Downloads/zipfiles')
+        elif file.endswith('.rpm'):
+            shutil.move(filepath, '/home/kelechi/Downloads/rpm')
+        elif file.endswith('.gif') or file.endswith('.svg') or :
+            shutil.move(filepath, '/home/kelechi/Downloads/gifs')
+            
 checkfile()
 
 
 
 
-
-
-
-
-
-
-
-
-
 # def checkfile():
+#     file_types = {
+#         '.txt': 'textfiles',
+#         '.jpg': 'Images',
+#         '.png': 'Images',
+#         '.pdf': 'PDFs',
+#         '.zip': 'zipfiles',
+#         '.rpm': 'rpm',
+#     }
+
 #     for file in download_files:
 #         filepath = os.path.join(downloads_folder, file)
-        
-#         if file.endswith('.txt'):
-#             shutil.move(filepath, '/home/kelechi/Downloads/textfiles')
-#         elif file.endswith('.jpg') or file.endswith('.png'):
-#             shutil.move(filepath, '/home/kelechi/Downloads/Images')
-#         elif file.endswith('.pdf'):
-#             shutil.move(filepath, '/home/kelechi/Downloads/PDFs')
-#         elif file.endswith('.zip'):
-#             shutil.move(filepath, '/home/kelechi/Downloads/zipfiles')
-#         elif file.endswith('.rpm'):
-#             shutil.move(filepath, '/home/kelechi/Downloads/rpm')
-#         else:
-#             shutil.move(filepath, '/home/kelechi/Downloads/others')
-            
+#         extension = os.path.splitext(file)[1]
+
+#         destination_folder = file_types.get(extension, 'others')
+#         destination_path = os.path.join(downloads_folder, destination_folder)
+
+#         shutil.move(filepath, destination_path)
+
 # checkfile()
